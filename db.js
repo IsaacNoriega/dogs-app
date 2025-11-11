c5fb57b40eb307256c3ae5e09ef9ed5e417b0780
Isaac Noriega <isaac.noriega@iteso.mx>
2025-11-10 21:06:36 -0600

    Si jala me rapo

--- db.js
+++ db.js
@@ -7,8 +7,8 @@ const { NODE_ENV } = process.env;
 // Configuraciones para cada entorno
 const DB_CONFIG = {
   local: {
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
