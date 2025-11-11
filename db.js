5010875d4783c6fcb76794fb87159387be1f3e29
Isaac Noriega <isaac.noriega@iteso.mx>
2025-11-10 20:24:58 -0600

    Si jala me rapo

--- db.js
+++ db.js
@@ -5,18 +5,18 @@ const { NODE_ENV } = process.env;
 // Configuraciones para cada entorno
 const DB_CONFIG = {
   local: {
-    host: process.env.LOCAL_DB_HOST || '127.0.0.1',
-    port: process.env.LOCAL_DB_PORT || 3306,
-    user: process.env.LOCAL_DB_USER || 'root',
-    password: process.env.LOCAL_DB_PASS || '',
-    database: process.env.LOCAL_DB_NAME || 'mydogs_local',
-  },
-  prod: {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER,
+    user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+
+  prod: {
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT || 3306,
+    user: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASS,
+    database: process.env.PROD_DB_NAME,
   },
 };
 
